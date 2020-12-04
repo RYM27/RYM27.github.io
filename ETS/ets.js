2898c35a2da76d6d8742e9c97b23a6e23e2bfdf3
RYM27 <ramayusuf99@yahoo.co.id>
2020-12-04 08:41:54 +0700

    Ubah Menjadi 3D

--- ETS/ets.js
+++ ETS/ets.js
@@ -7,25 +7,33 @@ var shaderProgram;
 
 var vertexPositionBuffer;
 
+var indexBuffer;
+
 var vertexColorBuffer;
 
 var mvMatrix = mat4.create();
 
+var projectionMatrix = mat4.create();
+
 var lastTime = 0;
 
+var posisiKamera = 0;
 
 class Planet
 {
-    constructor(nama, x, y, r, rotAngle, warnaR, warnaG, warnaB, putihTengah) {
+    constructor(nama, x, y, z, r, rotAngle, warnaR, warnaG, warnaB) {
         this.nama = nama;
         this.x = x;
         this.y = y;
+        this.z = z;
         this.r = r;
         this.rotAngle = rotAngle;
         this.warnaR = warnaR;
         this.warnaG = warnaG;
         this.warnaB = warnaB;
-        this.putihTengah = putihTengah;
+        this.vertices = []; 
+        this.indices = [];
+        this.load = 0;
 
         if(this.nama == "Bulan") {
             this.rotAngle2 = 0.0;
@@ -35,47 +43,117 @@ class Planet
     setupBuffers() {
         vertexPositionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    
-        const positions = [
-            this.x,  this.y // circle center vertex
-        ];
-        const stops = 100;
-        
-        for (var i = 0; i <= stops; i++){
-            positions.push(this.x + this.r * Math.cos(i * 2 * Math.PI/stops)); // x coord
-            positions.push(this.y + this.r * Math.sin(i * 2 * Math.PI/stops)); // y coord
+
+        if(this.load == 0) {
+            var SPHERE_DIV = 12;
+            var i, ai, si, ci;
+            var j, aj, sj, cj;
+            var p1, p2;
+
+            // Vertices
+            for (j = 0; j <= SPHERE_DIV; j++) {
+                aj = j * Math.PI / SPHERE_DIV;
+                sj = Math.sin(aj);
+                cj = Math.cos(aj);
+                for (i = 0; i <= SPHERE_DIV; i++) {
+                    ai = i * 2 * Math.PI / SPHERE_DIV;
+                    si = Math.sin(ai);
+                    ci = Math.cos(ai);
+
+                    this.vertices.push(si * sj);  // X
+                    this.vertices.push(cj);       // Y
+                    this.vertices.push(ci * sj);  // Z
+                } 
+            }
         }
-    
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
         
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vertices), gl.STATIC_DRAW);
+
+        if(this.load == 0) {
+            // Indices
+            indexBuffer = gl.createBuffer(); 
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+            for (j = 0; j < SPHERE_DIV; j++) {
+                for (i = 0; i < SPHERE_DIV; i++) {
+                p1 = j * (SPHERE_DIV+1) + i;
+                p2 = p1 + (SPHERE_DIV+1);
+
+                this.indices.push(p1);
+                this.indices.push(p2);
+                this.indices.push(p1 + 1);
+
+                this.indices.push(p1 + 1);
+                this.indices.push(p2);
+                this.indices.push(p2 + 1);
+                }
+            }
+        }
+        
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.indices), gl.STATIC_DRAW);
+
+        if(this.load == 0) {
+            this.colors = [];
+            for (i = 0; i <= 169; i++){
+                this.colors.push(this.warnaR/255.0,  this.warnaG/255.0,  this.warnaB/255.0, 1.0); 
+            }
+            this.load = 1;
+        }
+        //color
         vertexColorBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.STATIC_DRAW);
+    }
 
-        if(this.putihTengah){
-            var colors = [
-                1.0,  1.0,  1.0,  1.0, // rgba color
-            ];
-        }
-        else {
-            var colors = [
-                this.warnaR/255.0,  this.warnaG/255.0,  this.warnaB/255.0,  1.0, // rgba color
-            ];
-        }
+    draw() {
+        var numComponents = 3;
+        const type = gl.FLOAT;
+        const normalize = false;
+        const stride = 0;
+        const offset = 0;
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
+        gl.vertexAttribPointer(
+            shaderProgram.vertexPositionAttribute,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
+
+        var numComponents = 4;
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
+        gl.vertexAttribPointer(
+            shaderProgram.vertexColorAttribute,
+            numComponents,
+            type,
+            normalize,
+            stride,
+            offset);
         
-        for (i = 0; i <= 100; i++){
-            colors.push(this.warnaR/255.0,  this.warnaG/255.0,  this.warnaB/255.0, 1.0); 
-        }
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+        
+        //model view matrix
+        mat4.identity(mvMatrix);
+        mat4.translate(mvMatrix, mvMatrix, [this.x/this.z, this.y/this.z, this.z/this.z]);
+        mat4.scale(mvMatrix, mvMatrix, new Array(3).fill(this.r/26.0))
+        //mat4.rotateZ(mvMatrix, mvMatrix, degToRad(90));
+        //mat4.rotate(mvMatrix, mvMatrix, degToRad(45), [ 1.0, 0.0, 0.0 ]);
+        gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+
+        gl.drawElements(gl.TRIANGLES, this.indices.length, gl.UNSIGNED_SHORT, 0);
     }
 }
 
 /* Deklarasi Object Planet */
-var matahari = new Planet("Matahari", 0.0, 0.0, 0.2, 0.0, 255.0, 165.0, 0.0, true);
-var merkurius = new Planet("Merkurius", 0.4, 0.2, 0.06, 90.0, 210.0, 105.0, 30.0, false);
-var venus = new Planet("Venus", -0.5, 0.2, 0.06, 180.0, 255.0, 0.0, 0.0, true);
-var bumi = new Planet("Bumi", 0.05, -0.9, 0.08, 0.0, 0.0, 0.0, 255.0, true);
-var bulan = new Planet("Bulan", 0.1, -1.2, 0.03, 0.0, 105.0, 105.0, 105.0, false);
-var mars = new Planet("Mars", 1.2, 0.7, 0.06, 90.0, 165.0, 42.0, 42.0, true);
+var matahari = new Planet("Matahari", 0.0, 0.0, 25.0, 1.0, 0.0, 255.0, 165.0, 0.0);
+var merkurius = new Planet("Merkurius", 0.1, -2.0, 25.0, 0.3, 0.0, 210.0, 105.0, 30.0);
+var venus = new Planet("Venus", -2.5, 2.0, 25.0, 0.3, 180.0,  255.0, 0.0, 0.0);
+var bumi = new Planet("Bumi", 0.0, -5.0, 25.0, 0.3, 0.0, 0.0, 0.0, 255.0);
+var bulan = new Planet("Bulan", 0.0, -6.0, 25.0, 0.15, 0.0, 105.0, 105.0, 105.0);
+var mars = new Planet("Mars", -6.0, 4.0, 25.0, 0.3, 180.0, 165.0, 42.0, 42.0);
+var jupiter = new Planet("Jupiter", -8.0, 6.0, 25.0, 0.5, 180.0, 255.0, 255.0, 255.0);
+var saturnus = new Planet("Saturnus", -10.0, 5.0, 25.0, 0.4, 180.0, 255.0, 228.0 ,196.0);
+var uranus = new Planet("Uranus", 15.0, 5.0, 25.0, 0.35, 90.0, 95.0, 158.0, 160.0);
+var neptunus = new Planet("Neptunus", 0.0, -25.0, 25.0, 0.35, 0.0, 123.0, 104.0, 238.0);
 
 function degToRad(degrees)
 {
@@ -169,110 +247,123 @@ function setupShaders() {
 
     shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
 
-    shaderProgram.translation = gl.getUniformLocation(shaderProgram, "translation");
+    shaderProgram.projectionMatrix = gl.getUniformLocation(shaderProgram, "uProjectionMatrix");
 
-}
-
-
-/* Fungsi Draw */
-function draw() { 
-
-    var numComponents = 2;
-    const type = gl.FLOAT;
-    const normalize = false;
-    const stride = 0;
-    const offset = 0;
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexPositionBuffer);
-    gl.vertexAttribPointer(
-        shaderProgram.vertexPositionAttribute,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    
-    var numComponents = 4;
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorBuffer);
-    gl.vertexAttribPointer(
-        shaderProgram.vertexColorAttribute,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    
-    
-    mat4.identity(mvMatrix);
-
-    //mat4.rotateX(mvMatrix, mvMatrix, degToRad(rotAngle));
-    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-
-    gl.uniform4f(shaderProgram.translation, 0.0, 0.0, 0.0, 0.6);
-
-    const vertexCount = 102;
-    gl.drawArrays(gl.TRIANGLE_FAN, offset, vertexCount);
 }
 
 function tick() {
-    setTimeout(() => { requestAnimFrame(tick) }, 45);
+    //setTimeout(() => { requestAnimFrame(tick) }, 45);
+    requestAnimFrame(tick);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    matahari.setupBuffers()
-    draw();
-    merkurius.setupBuffers()
-    draw();
-    venus.setupBuffers()
-    draw();
-    bumi.setupBuffers()
-    draw();
-    bulan.setupBuffers()
-    draw();
-    mars.setupBuffers()
-    draw();
-   animate();
+    projection();
+    matahari.setupBuffers();
+    matahari.draw();
+    merkurius.setupBuffers();
+    merkurius.draw();
+    venus.setupBuffers();
+    venus.draw();
+    bumi.setupBuffers();
+    bumi.draw();
+    bulan.setupBuffers();
+    bulan.draw();
+    mars.setupBuffers();
+    mars.draw();
+    jupiter.setupBuffers();
+    jupiter.draw();
+    saturnus.setupBuffers();
+    saturnus.draw();
+    uranus.setupBuffers();
+    uranus.draw();
+    neptunus.setupBuffers();
+    neptunus.draw();
+    animate();
 }
 
 function animate() {
     var timeNow = new Date().getTime();
     if(lastTime != 0) {
         var elapsedTime = timeNow - lastTime;
-        merkurius.rotAngle = merkurius.rotAngle + 0.1 % 360;
-        venus.rotAngle = venus.rotAngle + 0.1 % 360;
-        bumi.rotAngle = bumi.rotAngle + 0.1 % 360;
-        bulan.rotAngle = bulan.rotAngle + 0.1 % 360;
-        bulan.rotAngle2 = bulan.rotAngle2 + 0.3 % 360;
-        mars.rotAngle = mars.rotAngle + 0.1 % 360;
+        merkurius.rotAngle = merkurius.rotAngle + 0.05 % 360;
+        venus.rotAngle = venus.rotAngle + 0.03 % 360;
+        bumi.rotAngle = bumi.rotAngle +  0.019 % 360;
+        bulan.rotAngle = bulan.rotAngle + 0.019 % 360;
+        bulan.rotAngle2 = bulan.rotAngle2 + 0.1 % 360;
+        mars.rotAngle = mars.rotAngle + 0.013 % 360;
+        jupiter.rotAngle = jupiter.rotAngle + 0.01 % 360;
+        saturnus.rotAngle = saturnus.rotAngle + 0.008 % 360;
+        uranus.rotAngle = uranus.rotAngle + 0.006 % 360;
+        neptunus.rotAngle = neptunus.rotAngle + 0.004 % 360;
 
-        merkurius.x = merkurius.x + 0.04 * Math.cos(merkurius.rotAngle);
-        merkurius.y = merkurius.y + 0.04 * Math.sin(merkurius.rotAngle);
+        merkurius.x = merkurius.x + 0.1 * Math.cos(merkurius.rotAngle);
+        merkurius.y = merkurius.y + 0.1 * Math.sin(merkurius.rotAngle);
 
-        venus.x = venus.x + 0.06 * Math.cos(venus.rotAngle);
-        venus.y = venus.y + 0.06 * Math.sin(venus.rotAngle);
+        venus.x = venus.x + 0.1 * Math.cos(venus.rotAngle);
+        venus.y = venus.y + 0.1 * Math.sin(venus.rotAngle);
 
-        bumi.x = bumi.x + 0.09 * Math.cos(bumi.rotAngle);
-        bumi.y = bumi.y + 0.09 * Math.sin(bumi.rotAngle);
+        bumi.x = bumi.x + 0.1 * Math.cos(bumi.rotAngle);
+        bumi.y = bumi.y + 0.1 * Math.sin(bumi.rotAngle);
 
-        bulan.x = bulan.x + 0.09 * Math.cos(bulan.rotAngle);
-        bulan.y = bulan.y + 0.09 * Math.sin(bulan.rotAngle);
+        bulan.x = bulan.x + 0.1 * Math.cos(bulan.rotAngle);
+        bulan.y = bulan.y + 0.1 * Math.sin(bulan.rotAngle);
 
-        bulan.x = bulan.x + 0.09 * Math.cos(bulan.rotAngle2);
-        bulan.y = bulan.y + 0.09 * Math.sin(bulan.rotAngle2);
+        bulan.x = bulan.x + 0.1 * Math.cos(bulan.rotAngle2);
+        bulan.y = bulan.y + 0.1 * Math.sin(bulan.rotAngle2);
 
-        mars.x = mars.x + 0.14 * Math.cos(mars.rotAngle);
-        mars.y = mars.y + 0.14 * Math.sin(mars.rotAngle);
-        
+        mars.x = mars.x + 0.1 * Math.cos(mars.rotAngle);
+        mars.y = mars.y + 0.1 * Math.sin(mars.rotAngle);
+
+        jupiter.x = jupiter.x + 0.1 * Math.cos(jupiter.rotAngle);
+        jupiter.y = jupiter.y + 0.1 * Math.sin(jupiter.rotAngle);
+
+        saturnus.x = saturnus.x + 0.1 * Math.cos(saturnus.rotAngle);
+        saturnus.y = saturnus.y + 0.1 * Math.sin(saturnus.rotAngle);
+
+        uranus.x = uranus.x + 0.1 * Math.cos(uranus.rotAngle);
+        uranus.y = uranus.y + 0.1 * Math.sin(uranus.rotAngle);
+
+        neptunus.x = neptunus.x + 0.1 * Math.cos(neptunus.rotAngle);
+        neptunus.y = neptunus.y + 0.1 * Math.sin(neptunus.rotAngle);
     }
     lastTime = timeNow;
+}
 
+function projection() {
+    canvas.addEventListener("mousedown", function(event){
+        if(posisiKamera == 0) {
+            posisiKamera = 1;
+        }
+        else if(posisiKamera == 1) {
+            posisiKamera = 0;
+        }
+    });
+    //projection matrix
+    mat4.identity(projectionMatrix);
+    if(posisiKamera == 0) {
+        mat4.translate(projectionMatrix, projectionMatrix, [0.0, 0.0, 0.0]);
+        mat4.rotateX(projectionMatrix, projectionMatrix, degToRad(0));
+        document.getElementById("posisiKamera").innerText = "Posisi Kamera: Top Down (Dari Atas)";
+    }
+    else if(posisiKamera == 1) {
+        mat4.translate(projectionMatrix, projectionMatrix, [0.0, 1.0, 0.0]);
+        mat4.rotateX(projectionMatrix, projectionMatrix, degToRad(90));
+        document.getElementById("posisiKamera").innerText = "Posisi Kamera: Dari Samping";
+    }
+    //mat4.perspective(projectionMatrix, degToRad(150), gl.canvas.clientWidth / gl.canvas.clientHeight, 0.0, 40000000000000000.0);
+    //mat4.lookAt(projectionMatrix, [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]);
+    //mat4.rotate(projectionMatrix, projectionMatrix, degToRad(45), [0.0, 1.0, 0.0 ]);
+    gl.uniformMatrix4fv(shaderProgram.projectionMatrix, false, projectionMatrix);
 }
 
 /* Fungsi yang dipanggil setelah page diload */
 function startup() {
-    
     canvas = document.getElementById("myCanvas");
     gl = createGLContext(canvas);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
     gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     setupShaders();
     tick();
 }
